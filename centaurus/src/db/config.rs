3f293216fec8d1215316ce59dfd7aba2fbc70588
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-10-23 18:41:45 +0200

    refactor: moved serde fns

--- centaurus/src/db/config.rs
+++ centaurus/src/db/config.rs
@@ -7,3 +7,14 @@ pub struct DBConfig {
   pub database_connect_timeout: u64,
   pub database_logging: bool,
 }
+
+impl Default for DBConfig {
+  fn default() -> Self {
+    Self {
+      database_max_connections: 1024,
+      database_min_connections: 1,
+      database_connect_timeout: 5,
+      database_logging: false,
+    }
+  }
+}
--- centaurus/src/serde.rs
+++ centaurus/src/serde.rs
@@ -14,3 +14,15 @@ where
     Some(s) => FromStr::from_str(s).map_err(de::Error::custom).map(Some),
   }
 }
+
+pub fn de_str<'de, D: Deserializer<'de>, T: FromStr>(d: D) -> Result<T, D::Error>
+where
+  T::Err: std::fmt::Display,
+{
+  let s = String::deserialize(d)?;
+  T::from_str(&s).map_err(serde::de::Error::custom)
+}
+
+pub fn se_str<T: ToString, S: serde::Serializer>(t: &T, s: S) -> Result<S::Ok, S::Error> {
+  s.serialize_str(&t.to_string())
+}
