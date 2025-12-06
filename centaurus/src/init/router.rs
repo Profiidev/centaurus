8c25ca86be46b22bc302fdc62a75efab153d3c99
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-12-06 23:17:39 +0100

    feat: added metrics and frontend state init

--- centaurus/src/init/router.rs
+++ centaurus/src/init/router.rs
@@ -1,12 +1,22 @@
 use axum::Router;
 
+#[cfg(feature = "metrics")]
+use crate::init::metrics::init_metrics;
 use crate::{config::BaseConfig, init::axum::add_base_layers, req::health};
 
-pub async fn base_router(api_router: Router, config: &BaseConfig) -> Router {
+pub async fn base_router(
+  api_router: Router,
+  config: &BaseConfig,
+  #[cfg(feature = "metrics")] metrics_name: String,
+  #[cfg(feature = "metrics")] extra_labels: Vec<(String, String)>,
+) -> Router {
+  #[cfg(feature = "metrics")]
+  let handle = init_metrics(metrics_name.clone());
+
   #[cfg(feature = "frontend")]
-  let router = crate::init::frontend::router();
+  let mut router = crate::init::frontend::router();
   #[cfg(not(feature = "frontend"))]
-  let router = Router::new();
+  let mut router = Router::new();
 
   #[cfg(not(feature = "metrics"))]
   let sub_router = api_router.merge(health::router());
@@ -20,8 +30,24 @@ pub async fn base_router(api_router: Router, config: &BaseConfig) -> Router {
     sub_router = sub_router.metrics_route().await;
   }
 
-  router
+  router = router
     .nest("/api", sub_router)
     .add_base_layers_filtered(config, |path| path.starts_with("/api"))
-    .await
+    .await;
+
+  #[cfg(feature = "frontend")]
+  {
+    use crate::init::frontend::frontend;
+
+    router = router.frontend().await;
+  }
+
+  #[cfg(feature = "metrics")]
+  {
+    use crate::init::metrics::metrics;
+
+    router = router.metrics(metrics_name, handle, extra_labels).await;
+  }
+
+  router
 }
