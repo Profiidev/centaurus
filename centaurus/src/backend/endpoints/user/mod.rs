4db7e2cc9f0a48026b0f93b2b43bced8e012c462
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-05-25 21:38:39 +0200

    feat: added email state init

--- centaurus/src/backend/endpoints/user/mod.rs
+++ centaurus/src/backend/endpoints/user/mod.rs
@@ -1,7 +1,9 @@
 use crate::backend::{
-  endpoints::websocket::state::UpdateMessage, middleware::rate_limiter::RateLimiter,
+  endpoints::{user::email::EmailChangeState, websocket::state::UpdateMessage},
+  middleware::rate_limiter::RateLimiter,
 };
 use aide::axum::ApiRouter;
+use axum::Extension;
 
 pub mod account;
 pub mod email;
@@ -14,4 +16,8 @@ pub fn router<T: UpdateMessage>(rate_limiter: &mut RateLimiter) -> ApiRouter {
     .nest("/account", account::router::<T>(rate_limiter))
     .nest("/info", info::router())
     .nest("/management", management::router::<T>())
+}
+
+pub fn state(router: ApiRouter) -> ApiRouter {
+  router.layer(Extension(EmailChangeState::init()))
 }
