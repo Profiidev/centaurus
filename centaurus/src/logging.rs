88445d5829132c979b736e0c48a38d73339203c4
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-05-26 11:06:22 +0200

    feat: added init for stderr logging

--- centaurus/src/logging.rs
+++ centaurus/src/logging.rs
@@ -1,11 +1,25 @@
 use tracing::level_filters::LevelFilter;
 use tracing_error::ErrorLayer;
-use tracing_subscriber::{Layer, layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{
+  Layer, fmt::writer::MakeWriter, layer::SubscriberExt, util::SubscriberInitExt,
+};
 
 pub fn init_logging(log_level: LevelFilter) {
+  init_logging_writer(log_level, std::io::stdout);
+}
+
+pub fn init_logging_stderr(log_level: LevelFilter) {
+  init_logging_writer(log_level, std::io::stderr);
+}
+
+pub fn init_logging_writer<W: for<'writer> MakeWriter<'writer> + Send + Sync + 'static>(
+  log_level: LevelFilter,
+  writer: W,
+) {
   color_eyre::install().expect("Failed to install color_eyre");
 
   let layer = tracing_subscriber::fmt::layer()
+    .with_writer(writer)
     .with_ansi(true)
     .with_filter(log_level);
 
