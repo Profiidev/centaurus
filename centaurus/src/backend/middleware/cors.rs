b8efd297c260183828decaa83fe55d6f980172bd
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-06-06 08:28:03 +0200

    fix: cors allow

--- centaurus/src/backend/middleware/cors.rs
+++ centaurus/src/backend/middleware/cors.rs
@@ -1,12 +1,21 @@
 use axum::http::HeaderValue;
-use http::Method;
+use http::{HeaderName, Method};
 use tower_http::cors::CorsLayer;
 
 use crate::{backend::config::BaseConfig, error::Result};
 
 pub fn cors(config: &BaseConfig) -> Result<CorsLayer> {
   let cors = CorsLayer::new()
-    .allow_methods([Method::GET, Method::POST])
+    .allow_methods([
+      Method::GET,
+      Method::POST,
+      Method::PUT,
+      Method::DELETE,
+      Method::HEAD,
+      Method::PATCH,
+      Method::OPTIONS,
+    ])
+    .allow_headers([crate::VERSION_HEADER_NAME.parse::<HeaderName>()?])
     .allow_credentials(true);
 
   let mut origins = Vec::new();
